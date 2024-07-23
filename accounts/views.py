85f567e40654c2676c1147f1a6b1598c15635fa0
valkorostyslav <enyway.valko.08.2004@gmail.com>
2024-07-23 10:59:20 +0300

    added create_user method

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+
+from accounts.models import Account
 from .forms import RegistrationForm
 
 # Create your views here.
 
 def register(request):
-    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number= form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name,
+                                               username=username, email=email, password=password)
+            user.phone_number = phone_number
+            user.save()
+    else:
+        form = RegistrationForm()
+        
     context = {
         'form': form,
     }
