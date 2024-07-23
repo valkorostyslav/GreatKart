1e974f9ffcc82760a849d59dd82c37da14f881fd
valkorostyslav <enyway.valko.08.2004@gmail.com>
2024-07-23 10:38:32 +0300

    added register.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
 
 # Create your views here.
+
+def register(request):
+    form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return
