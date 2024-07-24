6129ee8cbf89cbecdcce5e4fa0f7f52a74f4d43d
valkorostyslav <enyway.valko.08.2004@gmail.com>
2024-07-24 16:33:47 +0300

    added dashboard template and logout button

--- accounts/views.py
+++ accounts/views.py
@@ -62,8 +62,8 @@ def login(request):
         
         if user is not None:
             auth.login(request, user)
-            # messages.success(request, 'You are now logged in.')
-            return redirect('home')
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid loggin cradentials')
             return redirect('login')
@@ -90,3 +90,7 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, 'Invalid activation link')    
         return redirect('register')
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
