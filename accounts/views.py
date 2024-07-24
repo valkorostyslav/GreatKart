623a38c665d471ea335431facda7431551ed3344
valkorostyslav <enyway.valko.08.2004@gmail.com>
2024-07-24 16:04:07 +0300

    added send email to activate account

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from accounts.models import Account
 from .forms import RegistrationForm
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+
+# Verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
@@ -22,8 +31,21 @@ def register(request):
                                                username=username, email=email, password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request, 'Registration successful.')
-            return redirect('register')
+            
+            #USER ACTIVATION
+            current_site =  get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            # messages.success(request, 'Thank you for registering with us. We have sent you a verification email to your email address. Please verify it.')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
     context = {
@@ -52,3 +74,19 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out.')
     return redirect('login')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+        
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations! Your account is activated. ')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')    
+        return redirect('register')
--- greatkart/settings.py
+++ greatkart/settings.py
@@ -143,3 +143,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'enyway.valko.08.2004@gmail.com'
+EMAIL_HOST_PASSWORD = 'dmwwxnkkdazywxfm'
+EMAIL_USE_TLS = True
+
