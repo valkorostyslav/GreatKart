08d3c212a2acccdf75b7b05ebeb399b4214bac23
valkorostyslav <enyway.valko.08.2004@gmail.com>
2024-07-28 20:01:59 +0300

    added payments method(paypal)

--- orders/views.py
+++ orders/views.py
@@ -1,12 +1,32 @@
 import datetime
+import json
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from carts.models import CartItem
 from orders.forms import OrderForm
-from orders.models import Order
+from orders.models import Order, Payment
 
 # Create your views here.
+
+def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
+    
+    # Store transaction details inside Payment model
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status'],
+    )
+    payment.save()
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+    return render(request, 'orders/payments.html')
+
 
 def place_order(request, total=0, quantity=0):
     current_user = request.user
@@ -54,6 +74,16 @@ def place_order(request, total=0, quantity=0):
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
-            return redirect('checkout')
+            
+            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax': tax,
+                'grand_total': grand_total,
+            }
+            return render(request,  'orders/payments.html', context)
     else:
         return redirect('checkout')
+    
