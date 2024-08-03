8d75d333ac108fdf615fe99f3ebbabcf0005c6af
valkorostyslav <enyway.valko.08.2004@gmail.com>
2024-08-03 15:19:54 +0300

    fix profile picture in dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -154,8 +154,12 @@ def activate(request, uidb64, token):
 def dashboard(request):
     orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
     orders_count = orders.count()
+    
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
+    
     context = {
         'orders_count': orders_count,
+        'userprofile': userprofile,
     }
     return render(request, 'accounts/dashboard.html', context)
 
