4c1ce568d5f09c0a19b837b60e965ea83fe4f8cc
valkorostyslav <enyway.valko.08.2004@gmail.com>
2024-08-04 11:42:21 +0300

    update products and modify product detail page

--- store/views.py
+++ store/views.py
@@ -7,7 +7,7 @@ from carts.views import _cart_id
 from category.models import Category
 from orders.models import OrderProduct
 from store.forms import ReviewForm
-from .models import Product, ReviewRating
+from .models import Product, ProductGallery, ReviewRating
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.contrib import messages
 
@@ -55,11 +55,15 @@ def product_detail(request, category_slug, product_slug):
     # Get he reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
     
+    # Get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+    
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
         'orderproduct': orderproduct,
         'reviews': reviews,
+        'product_gallery': product_gallery,
     }
     return render(request, 'store/product_detail.html', context)
 
