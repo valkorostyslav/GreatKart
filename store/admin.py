fbc1f80efbaa91ded0eca6f7cc58b2ab61920446
valkorostyslav <146643351+valkorostyslav@users.noreply.github.com>
2025-11-30 14:02:58 +0200

    feat: add variation inline to product admin

--- store/admin.py
+++ store/admin.py
@@ -9,10 +9,14 @@ class ProductGalleryInLine(admin.TabularInline):
     model = ProductGallery
     extra = 1
 
+class VariationInline(admin.TabularInline):
+    model = Variation
+    extra = 1
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available',)
     prepopulated_fields = {'slug': ('product_name',)}
-    inlines = [ProductGalleryInLine]
+    inlines = [ProductGalleryInLine, VariationInline]
     
 class VariationAdmin(admin.ModelAdmin):
     list_display = ('product', 'variation_category', 'variation_value', 'is_active',)
